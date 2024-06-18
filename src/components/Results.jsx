35fcbec938fb72c08f4526c1773d3e55a90f3abe
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-06-18 16:06:01 +0300

    fix

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,6 @@ export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
   const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
 
-  console.log(resultsData);
   return (
     <table id="result">
       <thead>
